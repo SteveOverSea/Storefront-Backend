c7f263434b5d65173e31e2d72bb7bb15d7c3cf28
Stefan Rausch <rausch.stefan@edumail.at>
2021-05-24 11:58:23 +0200

    Add example products on products table up

--- migrations/sqls/20210510160240-products-table-up.sql
+++ migrations/sqls/20210510160240-products-table-up.sql
@@ -1,1 +1,4 @@
 CREATE TABLE products (id SERIAL PRIMARY KEY, name VARCHAR, price NUMERIC(8,2), description text, url text);
+INSERT INTO products (name, price, description, url) VALUES('Video Tape', 14.49, 'Enjoy your crisp on reality!', 'https://images.unsplash.com/photo-1546673653-36f99eb731bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8');
+INSERT INTO products (name, price, description, url) VALUES('Chair', 69.99, 'To get you grounded on those bold days!', 'https://images.unsplash.com/photo-1503602642458-232111445657?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8');
+INSERT INTO products (name, price, description, url) VALUES('Beer', 1.99, 'The best of all time!', 'https://images.unsplash.com/photo-1549231482-5cf39d19fba4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8');
